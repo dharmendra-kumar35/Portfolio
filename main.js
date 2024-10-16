60f47d422a042e5d4c02afdfd85bc6ecefad0728
Dharmendra Kumar <142709527+dharmendra-kumar35@users.noreply.github.com>
2024-10-17 00:59:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Web Developer", "Java Developer", "Frontend Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
